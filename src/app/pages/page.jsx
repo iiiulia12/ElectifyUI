6fe972dda177f66864fd372bc5bbd5f53edd0a8d
iiiulia12 <ratiuiulia@yahoo.com>
2025-02-21 02:57:46 +0200

    WIP - smart contract/account connect

--- src/app/pages/page.jsx
+++ src/app/pages/page.jsx
@@ -1,2 +1,45 @@
-const Pages = () => <div className={'text-2xl text-red-500 bg-amber-300'}> dana</div>
+'use client'
+
+import { contract } from 'contract'
+import { useEffect, useState } from 'react'
+import { useAccounts } from 'contract/useAccounts'
+
+const Pages = () => {
+  const [votes, setVotes] = useState([])
+
+  const account = useAccounts()
+  // const handleClick = async () => {
+  //   try {
+  //     const votes = await contract()
+  //     setVotes(votes)
+  //
+  //     console.log('votes', votes)
+  //   } catch (error) {
+  //     console.error('Error calling contract:', error)
+  //   }
+  // }
+
+  useEffect(() => {
+    const subscription = async () => {
+      contract(setVotes)
+    }
+
+    subscription()
+  }, [])
+  console.log(votes)
+
+  return (
+    <>
+      {/*<div className={'text-2xl text-red-500 bg-amber-300 hover:text-white cursor-pointer'}>get votes</div>*/}
+      {/*{!isEmpty(votes) &&*/}
+      {/*  votes.map((vote, index) => (*/}
+      {/*    <div key={index}>*/}
+      {/*      {vote.name} {vote.numberOfVotes}*/}
+      {/*    </div>*/}
+      {/*  ))}*/}
+      <div> {account} </div>
+    </>
+  )
+}
+
 export default Pages
