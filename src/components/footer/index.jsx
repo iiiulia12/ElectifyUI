aa18a4779813b0c3cdd5bf67c656334d4c7387df
iiiulia12 <ratiuiulia@yahoo.com>
2025-05-28 14:45:50 +0300

    fix: footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,3 +1,1 @@
-export const Footer = () => (
-  <div className={'fixed bg-blackish-blue/80  flex-row w-full h-[7vh] bottom-0 '}> footer</div>
-)
+export const Footer = () => <div className={'bg-blackish-blue/80  flex-row w-full h-[7vh]'}> footer</div>
