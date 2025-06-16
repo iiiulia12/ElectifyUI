442d75591c8da9eb750fc839a6d38cc7a6c94d70
iiiulia12 <ratiuiulia@yahoo.com>
2025-06-17 02:35:07 +0300

    feat: enhance header text

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,10 +5,15 @@ export const Header = () => (
   <div className={'fixed top-0 w-full z-50'}>
     <div className={'w-full top-0 absolute h-[9vh]  backdrop-blur-sm'} />
 
-    <div className={' absolute bg-cloudy/5 text-blackish-blue/70 flex flex-row w-full  h-[9vh] items-center px-6'}>
-      <div className={'text-2xl font-bold mr-8'}>Electify</div>
+    <div className={' absolute bg-cloudy/10 text-cloudy flex flex-row w-full  h-[9vh] items-center px-6'}>
+      <div
+        className={
+          'text-2xl rounded-lg px-4  border-mr-8 text-transparent bg-gradient-to-r from-deep-ocean  to-deep-ocean/60 bg-clip-text font-extrabold'
+        }>
+        Electify
+      </div>
 
-      <div className={'flex items-center space-x-4'}>
+      <div className={'flex items-center space-x-4 text-lg'}>
         <Link href={'/'} className={'hover:text-deep-ocean transition-colors'}>
           Home
         </Link>
